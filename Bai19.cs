6e9ffae983f3269aeb68310efe20d9188793bed5
thanhvinh <vinhyhan@gmail.com>
2026-03-20 23:50:41 +0700

    Bai19

---
+++ Bai19.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("PhamTranThanhVinh");
+        Console.WriteLine("2415053122248");
+        Console.WriteLine("225THLTC#04");
+        var nums = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+
+        var result = nums.GroupBy(n => n)
+                         .OrderByDescending(g => g.Count())
+                         .First().Key;
+
+        Console.WriteLine(result);
+    }
+}
