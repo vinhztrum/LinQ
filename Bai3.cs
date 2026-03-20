2c32167d5a46ce70e2bd8e89796cf417d299aa3b
thanhvinh <vinhyhan@gmail.com>
2026-03-20 23:37:42 +0700

    Bai3

---
+++ Bai3.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("225THLTC#04");
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+var result = numbers.OrderBy(n => n);
+
+Console.WriteLine(string.Join(" ", result));
