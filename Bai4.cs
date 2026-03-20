4c563a9c4695b28a1b4fbfd0574bd9ee3696d6b0
thanhvinh <vinhyhan@gmail.com>
2026-03-20 23:38:59 +0700

    Bai4

---
+++ Bai4.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("225THLTC#04");
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var result = numbers.OrderByDescending(n => n);
+Console.WriteLine(string.Join(" ", result));
