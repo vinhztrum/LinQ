2b7fa96cca964130735f10609a54934bbacbddbd
thanhvinh <vinhyhan@gmail.com>
2026-03-20 23:46:00 +0700

    Bai15

---
+++ Bai15.cs
@@ -0,0 +1,15 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("PhamTranThanhVinh");
+        Console.WriteLine("2415053122248");
+        Console.WriteLine("225THLTC#04");
+        var nums = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+
+        var result = nums.Distinct();
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
