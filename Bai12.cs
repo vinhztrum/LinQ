9a560d652ad67375e4dd652af8bf0afe2a3c3916
thanhvinh <vinhyhan@gmail.com>
2026-03-20 23:45:00 +0700

    Bai12

---
+++ Bai12.cs
@@ -0,0 +1,28 @@
+﻿
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("PhamTranThanhVinh");
+        Console.WriteLine("2415053122248");
+        Console.WriteLine("225THLTC#04");
+        var students = new List<Student>()
+        {
+            new Student{Id=1, Name="An", Score=8},
+            new Student{Id=2, Name="Binh", Score=6},
+            new Student{Id=3, Name="Chi", Score=9},
+            new Student{Id=4, Name="Dung", Score=7}
+        };
+
+        int count = students.Count(s => s.Score >= 7);
+
+        Console.WriteLine(count);
+    }
+}
