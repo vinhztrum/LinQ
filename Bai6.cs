94413b8c484366cf8cfe6ff9fb5a6544c9f730e6
thanhvinh <vinhyhan@gmail.com>
2026-03-20 23:42:20 +0700

    Bai6

---
+++ Bai6.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("225THLTC#04");
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+int count = numbers.Count(n => n % 2 == 0);
+Console.WriteLine(count);
