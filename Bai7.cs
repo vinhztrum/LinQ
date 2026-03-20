e3c611ee35a31b8233349919adbb4d4c4b6c358f
thanhvinh <vinhyhan@gmail.com>
2026-03-20 23:42:55 +0700

    Bai7

---
+++ Bai7.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("225THLTC#04");
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+bool check = numbers.Any(n => n > 10);
+Console.WriteLine(check);
